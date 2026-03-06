fdb540262cee77c08cf4437e28be7e2a8f564c5a
Rainy <doge20162016@gmail.com>
2026-03-06 13:55:48 +0700

    Ознакомление с QFileInfo

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,10 +1,44 @@
 #include <QCoreApplication>
 #include <QFileInfo>
+#include <QTextStream>
+#include <iostream>
+#include <QString>
+
+
+
+
+
+//using namespace std;
+
+QTextStream cin(stdin);
+QTextStream cout(stdout);
+
 
 int main(int argc, char *argv[])
 {
-    QFileInfo
+    QString path;
+    QTextStream stream;
+
+
+    cout<<"Enter path to file: ";
+    cout.flush();
+
+    path = cin.readLine();
+    QFileInfo file(path);
+
+    if(file.isDir()){
+        cout<<"Error: not a file.\n"<<Qt::endl;
+        //cout.flush();
+        // H:\Documents
+    }
+
+    cout.flush();
+
+
+
     //QCoreApplication a(argc, argv);
 
     //return a.exec();
+    //system("pause");
+        return 0;
 }
