771b3ad5b30f05d7e09025a108af3883a23598a3
Rainy_ Morning <doge20162016@gmail.com>
2026-03-20 13:22:06 +0700

    Начало написания класса для мониторинга файлов

--- Lab1/FileMonitor.h
+++ Lab1/FileMonitor.h
@@ -2,3 +2,43 @@
 #define FILEMONITOR_H
 
 #endif // FILEMONITOR_H
+
+#include <QString>
+#include <QObject>
+#include <QFileInfo>
+#include <QVector>
+
+class FileMonitor : public QObject{
+
+    Q_OBJECT
+
+    QVector <QFileInfo> Files;
+
+public:
+
+    // конструктор по умолчанию
+    FileMonitor(){}
+
+    // конструктор по одной строке к одному файлу
+    FileMonitor(const QString &QStrPath){
+        QFileInfo fileinfo(QStrPath);
+        Files.push_back(fileinfo);
+    }
+
+    // конструктор по вектору строк к файлам
+    FileMonitor(QVector <QString> &QStrPaths){
+        for(QString path : QStrPaths){
+            Files.push_back(QFileInfo(path));
+        }
+    }
+
+signals:
+    // изменение имени или удаление файла равноценно - изменить путь к файлу
+    // значит файл изменился, значит файл по прошлому пути можно считать удалённым\утерянным
+    void FileLost();
+
+    void SizeChanged(int newSize);
+
+
+
+};
