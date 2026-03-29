dba62f222c08eebb8424fe59e5525b6030ecba98
Rainy_ Morning <doge20162016@gmail.com>
2026-03-30 00:15:01 +0700

    Расширение функционала FileList
    - getter List
    - add_path() которая добавляет в List новые пути и не добавляет дубликаты
    - QString & operator[] - оператор индекса в List
    - корректировка тестовых данных
    - вынесение из .cpp кодов исключений в .h файл
    (обработка исключений предполагается вне класса)

--- Lab1/FileList.h
+++ Lab1/FileList.h
@@ -6,11 +6,21 @@
 #include "IFileList.h"
 #include <QFile>
 
+// коды возможных ошибок(исключений)
+#define EXCEPTION_UNABLE_TO_OPEN_FILE 101
+#define EXCEPTION_INDEX_OUT_OF_BOUNDS 102
+
 class FileList: public IFileList{
 private:
     // Список путей к файлам
     QVector <QString> List;
 
+
+
+    // добавить в список путь к файлу (return true)
+    // если файл уже есть, то не добавлять (return false)
+    /// (будет использоваться в FileList::refreshList(...)) и конструкторах
+    bool add_path(const QString &path);
 public:
 
     FileList(){}
@@ -30,8 +40,14 @@ public:
     void refreshList(const QString &new_path_to_File_with_List);
     void refreshList(const QVector <QString> &new_paths);
 
+    // getter списка
+    const QVector <QString> &get_list() const;
+
+    // оператор[]
+    QString & operator[](const unsigned int &index);
 
 
-     ~FileList();
+
+    ~FileList() {}
 };
 
